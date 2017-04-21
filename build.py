3ccf292ec2169418b8b4838ce4ab654dc7ddccd3
Nicholas Escalona <nicholas.escalona@gmail.com>
2017-04-21 12:56:41 -0400

    build.py pulls in landed_titles and localisation

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 
 import shutil
 from ck2parser import (rootpath, SimpleParser, get_cultures, is_codename, Pair,
-                       csv_rows)
+                       csv_rows, vanilladir)
 from print_time import print_time
 
 def ensure_empty(path):
@@ -10,14 +10,12 @@ def ensure_empty(path):
         shutil.rmtree(str(path))
     path.mkdir(parents=True)
 
-def build_landed_titles(parser, mod, tagged_titles):
-    outdir = (rootpath / ('Converter/Converter+' + mod.name) /
-              'common/landed_titles')
+def build_landed_titles(parser, outdir, tagged_titles):
+    outdir /= 'common/landed_titles'
     ensure_empty(outdir)
-    parser.moddirs = [mod]
     cultures = set(get_cultures(parser, groups=False))
-    for path, tree in parser.parse_files('common/landed_titles/*', mod):
-        if 'override' in path.name:
+    for path, tree in parser.parse_files('common/landed_titles/*'):
+        if vanilladir in path.parents or 'override' in path.name:
             continue
         diff = False
         dfs = list(tree)
@@ -36,24 +34,39 @@ def build_landed_titles(parser, mod, tagged_titles):
         if diff:
             parser.write(tree, outdir / path.name)
 
+def copy_localisation(mods, outdir):
+    outdir /= 'localisation'
+    outdir.mkdir(parents=True, exist_ok=True)
+    for path in outdir.glob('localisation/*.csv'):
+        if path.name != 'converter.csv':
+            path.unlink()
+    for mod in mods:
+        for path in mod.glob('localisation/*.csv'):
+            shutil.copy(str(path), str(outdir / path.name))
+
 @print_time
 def main():
     swmh = rootpath / 'SWMH-BETA/SWMH'
     emf = rootpath / 'EMF/EMF'
     emf_swmh = rootpath / 'EMF/EMF+SWMH'
     mini = rootpath / 'MiniSWMH/MiniSWMH'
-    converter = rootpath / 'Converter'
+    sed = rootpath / 'sed2/build/SED2'
+    sed_emf = rootpath / 'sed2/build/SED2+EMF'
+    converter = rootpath / 'Converter/Converter'
+    converter_mini = rootpath / 'Converter/Converter+Mini'
     parser = SimpleParser()
 
     tagged_titles = set()
-    nation_table = converter / 'Converter/eu4_converter/nation_table.csv'
-    for title, *_ in csv_rows(nation_table):
+    for title, *_ in csv_rows(converter / 'eu4_converter/nation_table.csv'):
         if title[0] in 'ekd':
             tagged_titles.add(title)
 
-    build_landed_titles(parser, swmh, tagged_titles)
-    build_landed_titles(parser, emf, tagged_titles)
-    build_landed_titles(parser, emf_swmh, tagged_titles)
+    parser.moddirs = [swmh, emf, emf_swmh]
+    build_landed_titles(parser, converter, tagged_titles)
+    parser.moddirs = [mini]
+    build_landed_titles(parser, converter_mini, tagged_titles)
+
+    copy_localisation([sed, sed_emf], converter)
 
 if __name__ == '__main__':
     main()
